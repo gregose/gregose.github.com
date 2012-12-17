8fb62f9fa39e1c8617574fccf5b20ced5335f744
Greg Ose <greg@nullmethod.com>
2012-12-16 18:36:52 -0600

    updated blog dir index

--- javascripts/tabs.js
+++ javascripts/tabs.js
@@ -6,38 +6,4 @@
 * http://www.opensource.org/licenses/mit-license.php
 * 8/17/2011
 */
-
-
-
-$(document).ready(function() {
-
-	/* Tabs Activiation
-	================================================== */
-
-	var tabs = $('ul.tabs');
-
-	tabs.each(function(i) {
-
-		//Get all tabs
-		var tab = $(this).find('> li > a');
-		tab.click(function(e) {
-
-			//Get Location of tab's content
-			var contentLocation = $(this).attr('href');
-
-			//Let go if not a hashed one
-			if(contentLocation.charAt(0)=="#") {
-
-				e.preventDefault();
-
-				//Make Tab Active
-				tab.removeClass('active');
-				$(this).addClass('active');
-
-				//Show Tab Content & add active class
-				$(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
-
-			}
-		});
-	});
-});
+$(document).ready(function(){var e=$("ul.tabs");e.each(function(e){var t=$(this).find("> li > a");t.click(function(e){var n=$(this).attr("href");n.charAt(0)=="#"&&(e.preventDefault(),t.removeClass("active"),$(this).addClass("active"),$(n).show().addClass("active").siblings().hide().removeClass("active"))})})});
